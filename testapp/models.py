804dbc3fc612212d8ee9efb6e2ce3a0b31406936
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2023-04-11 23:16:15 +0900

    add: migration

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # ユーザID
+    user_id = models.IntegerField()
+    # ユーザ名
+    user_name = models.CharField(max_length=128)
+    # 登録日
+    registered_at = models.DateTimeField()
